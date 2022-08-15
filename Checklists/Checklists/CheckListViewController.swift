f6b567e2600f62050df640d25b8d3586b1e31304
Ahmet Hakan Demirel <05190000043@ogrenci.ege.edu.tr>
2022-08-15 15:57:19 +0300

    data model

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -8,10 +8,51 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    var items = [ChecklistItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let item1 = ChecklistItem()
+        item1.text = "Walk the dog"
+        items.append(item1)
+
+        let item2 = ChecklistItem()
+        item2.text = "Brush my teeth"
+        item2.checked = true
+        items.append(item2)
+
+        let item3 = ChecklistItem()
+        item3.text = "Learn iOS development"
+        item3.checked = true
+        items.append(item3)
+
+        let item4 = ChecklistItem()
+        item4.text = "Soccer practice"
+        items.append(item4)
+
+        let item5 = ChecklistItem()
+        item5.text = "Eat ice cream"
+        items.append(item5)
+    }
+    
+    func configureCheckmark(
+        for cell: UITableViewCell,
+        with item: ChecklistItem
+    ) {
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+
+    func configureText(
+        for cell: UITableViewCell,
+        with item: ChecklistItem
+    ) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
     }
     
     // MARK: - Table View Data Source
@@ -19,7 +60,7 @@ class ChecklistViewController: UITableViewController {
       _ tableView: UITableView,
       numberOfRowsInSection section: Int
     ) -> Int {
-      return 5
+        return items.count
     }
     
     // MARK: - Table View Delegate
@@ -28,11 +69,10 @@ class ChecklistViewController: UITableViewController {
       didSelectRowAt indexPath: IndexPath
     ) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
-              cell.accessoryType = .checkmark
-            } else {
-              cell.accessoryType = .none
-            }
+            let item = items[indexPath.row]
+            item.checked.toggle()
+            
+            configureCheckmark(for: cell, with: item)
           }
       tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -46,22 +86,14 @@ class ChecklistViewController: UITableViewController {
         withIdentifier: "ChecklistItem",
         for: indexPath)
         
-        let label = cell.viewWithTag(1000) as! UILabel
-
-          if indexPath.row == 0 {
-            label.text = "Walk the dog"
-          } else if indexPath.row == 1 {
-            label.text = "Brush my teeth"
-          } else if indexPath.row == 2 {
-            label.text = "Learn iOS development"
-          } else if indexPath.row == 3 {
-            label.text = "Soccer practice"
-          } else if indexPath.row == 4 {
-            label.text = "Eat ice cream"
-          }
+        let item = items[indexPath.row]
+        
+        configureText(for: cell, with: item)
+        configureCheckmark(for: cell, with: item)
         
       return cell
     }
+    
 
 
 
---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Ahmet on 15.08.2022.
+//
+
+import Foundation
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
