5e08aaee31bcb07a72c968730c492dda878da262
Ahmet Hakan Demirel <05190000043@ogrenci.ege.edu.tr>
2022-08-15 17:57:19 +0300

    buttons

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -107,6 +107,20 @@ class ChecklistViewController: UITableViewController {
       return cell
     }
     
+    // MARK: - Swipe to Delete
+    
+    override func tableView(
+      _ tableView: UITableView,
+      commit editingStyle: UITableViewCell.EditingStyle,
+      forRowAt indexPath: IndexPath
+    ) {
+      
+      items.remove(at: indexPath.row)
+
+      let indexPaths = [indexPath]
+      tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
 
 
 
